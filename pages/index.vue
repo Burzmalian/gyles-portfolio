1516ba370fa40e87d43f7de8b654c6aa314e6d8e
Burzmalian <gylesfohl@gmail.com>
2017-10-03 19:09:47 -0500

    test

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
         gyles-portfolio
       </h1>
       <h2 class="subtitle">
-        Nuxt.js project
+        Gyles
       </h2>
       <div class="links">
         <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
